23d74f4485b3222c74125d541eba4485cf7e5e3c
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2021-02-27 16:42:03 +0000

    Add files via upload

---
+++ VegetableMarket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegPriceKg = double.Parse(Console.ReadLine());
+            double fruitPriceKg = double.Parse(Console.ReadLine());
+            double totalKgVeg = double.Parse(Console.ReadLine());
+            double totalKgFruit = double.Parse(Console.ReadLine());
+
+            double totalPriceVeg = vegPriceKg * totalKgVeg;
+            double totalPriceFruit = fruitPriceKg * totalKgFruit;
+            double totalMoney = totalPriceFruit + totalPriceVeg;
+            double priceInEuro = totalMoney / 1.94;
+
+            Console.WriteLine($"{priceInEuro:f2}");
+        }
+    }
+}
